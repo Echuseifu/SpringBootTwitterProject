44e7ecaf24219752937f12d540947477f897ee5c
Echu Seifu <eseifu2016@gmail.com>
2021-06-01 11:31:42 -0400

    update

---
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/controller/TweetController.java
@@ -0,0 +1,54 @@
+package com.tts.TechTalentTwitter.controller;
+
+import com.tts.TechTalentTwitter.model.Tweet;
+import com.tts.TechTalentTwitter.model.User;
+import com.tts.TechTalentTwitter.service.TweetService;
+import com.tts.TechTalentTwitter.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@Controller
+public class TweetController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private TweetService tweetService;
+
+    // allow us to get all tweets
+    @GetMapping(value= {"/tweets", "/"})
+    public String getFeed(Model model){
+        List<Tweet> tweets = tweetService.findAll();
+        model.addAttribute("tweetList", tweets);
+        return "feed";
+    }
+
+    // will allow us to serve up the newTweet page
+    @GetMapping(value = "/tweets/new")
+    public String getTweetForm(Model model) {
+        model.addAttribute("tweet", new Tweet());
+        return "newTweet";
+    }
+
+    // handle the forms submission from the newTweet page
+    // it gets the logged in user and associates them with the tweet
+    @PostMapping(value = "/tweets")
+    public String submitTweetForm(@Valid Tweet tweet, BindingResult bindingResult, Model model) {
+        User user = userService.getLoggedInUser();
+        if (!bindingResult.hasErrors()) {
+            tweet.setUser(user);
+            tweetService.save(tweet);
+            model.addAttribute("successMessage", "Tweet successfully created!");
+            model.addAttribute("tweet", new Tweet());
+        }
+        return "newTweet";
+    }
+
+}
--- TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/service/UserService.java
+++ TechTalentTwitter/src/main/java/com/tts/TechTalentTwitter/service/UserService.java
@@ -1,10 +1,17 @@
 package com.tts.TechTalentTwitter.service;
 
+import com.tts.TechTalentTwitter.model.Role;
+import com.tts.TechTalentTwitter.model.User;
 import com.tts.TechTalentTwitter.repository.RoleRepository;
 import com.tts.TechTalentTwitter.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class UserService {
@@ -21,4 +28,43 @@ public class UserService {
         this.roleRepository = roleRepository;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
+
+
+    //// page 30
+
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public List<User> findAll(){
+        return (List<User>) userRepository.findAll();
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
+
+
+    // 31
+
+    public User saveNewUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setActive(1);
+        Role userRole = roleRepository.findByRole("USER");
+        user.setRoles(new HashSet<Role>(Arrays.asList(userRole)));
+        return userRepository.save(user);
+    }
+
+
+    //32
+
+    public User getLoggedInUser() {
+        String loggedInUsername = SecurityContextHolder.
+                getContext().getAuthentication().getName();
+
+        return findByUsername(loggedInUsername);
+    }
+
+
+
 }
